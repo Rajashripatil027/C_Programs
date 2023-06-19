8b4f4144e885ba98847502828cbf4c8da845822c
PATIL RAJASHRI <137038273+Rajashripatil027@users.noreply.github.com>
2023-06-19 12:30:27 +0530

    Add files via upload

---
+++ Basic_Prgorams/01.Hello Programing World.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+            printf("\n\t\t\t========================================\n");
+
+            printf("\n\t\t\t\tHello Programing World \n");
+
+            printf("\n\t\t\t========================================\n");
+
+            getch();
+            return 0;
+}
